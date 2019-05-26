1af47d37898a2f986c239dcbebbfe814d0f05f1a
marrodri <laza.mar64@gmail.com>
2019-05-25 17:16:58 -0700

    winset test

---
+++ test_maps/fdf_set_win.c
@@ -0,0 +1,27 @@
+
+#include "fdf_header.h"
+
+void set_win_sz(t_app **st_app, t_map *st_map)
+{
+	int buff;
+
+	buff = 0;
+	(*st_app)->x_win_sz = 0;
+	(*st_app)->y_win_sz = 0;
+	while (buff < ((st_map->x) * (st_map->z)))
+	{
+		if ((*st_app)->vert_buff[buff].x > (*st_app)->x_win_sz)
+		{
+			(*st_app)->x_win_sz = (*st_app)->vert_buff[buff].x;
+		}
+		if ((*st_app)->vert_buff[buff].z > (*st_app)->y_win_sz)
+		{
+			(*st_app)->y_win_sz = (*st_app)->vert_buff[buff].z;
+		}
+		buff++;
+	}
+	if ((*st_app)->x_win_sz > WIN_SZ)
+		(*st_app)->x_win_sz = WIN_SZ;
+	if ((*st_app)->y_win_sz > WIN_SZ)
+		(*st_app)->y_win_sz = WIN_SZ;
+}
